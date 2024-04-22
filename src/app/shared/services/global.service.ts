3c4abe9daccb9b9390fb2f955cb554b9cd0ec9fa
aryawaran4 <aryawaranggana.susilo@gmail.com>
2024-04-22 10:13:17 +0700

    add initial state local storage

--- src/app/shared/services/global.service.ts
+++ src/app/shared/services/global.service.ts
@@ -16,13 +16,37 @@ export class GlobalService {
   }
 
   getMe(): UserType {
-    const userString = localStorage.getItem('user') || '{}';
+    let userString = localStorage.getItem('user') || '{}';
+    if (userString === '{}') {
+      const defaultUserData = [{
+        email: 'dummy@gmail.com',
+        username: 'dummy',
+        password: '123456',
+        uuid: 'dummy',
+        createdAt: '2024-04-22T03:01:17.767',
+      }];// Set users data to empty array string if not found
+      localStorage.setItem('user', JSON.stringify(defaultUserData));
+      userString = JSON.stringify(defaultUserData);
+    }
     return JSON.parse(userString) as UserType;
   }
 
   getUsersData(): UserFavouriteType {
     const uuid = this.getMe().uuid
-    const usersData = localStorage.getItem('usersData') || '{}';
+    let usersData = localStorage.getItem('usersData') || '{}';
+    if (usersData === '{}') {
+      const defaultUserData = [{
+        username: 'dummy',
+        email: 'dummy@gmail.com',
+        uuid: 'dummy',
+        favourites: {
+          movies: [],
+          tv: []
+        }
+      }];// Set users data to empty array string if not found
+      localStorage.setItem('usersData', JSON.stringify(defaultUserData));
+      usersData = JSON.stringify(defaultUserData);
+    }
     return JSON.parse(usersData).find((user: { uuid: string; }) => user.uuid === uuid)
   }
 }
